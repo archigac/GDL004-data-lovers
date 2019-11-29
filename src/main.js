8efade7d666b4eebff58a6d0f0a2649a52a6c605
archigac <alejandra.arechiga.cervantes@gmail.com>
2019-11-29 11:02:41 -0600

    Se agregaron funciones de filtrado gradual de los personajes.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
-/**
- * import POKEMON from './data/pokemon/pokemon.js'
- * import LoL from './data/lol/lol.js'
- * import POTTER from './data/potter/potter.js'
- */
+"use strict";
 
- import { POTTER } from './data/potter/potter.js';
+import loadFilters from './js/loadFilters.js';
+import bindFilter from './js/bindFilters.js'
 
- console.log(POTTER);
+const main = () => {
+  loadFilters();
+  bindFilter();
+}
 
-/*
- * console.log(POKEMON);
- * console.log(LoL);
- * console.log(POTTER)
-*/
+main();
